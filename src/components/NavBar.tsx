c0151ef5c3aa957944b041bd1a2dc13f8e44b8d7
Rick <rick.kohjiaxuan@gmail.com>
2024-02-09 23:28:09 +0800

    Add authentication handling to NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ import {
 } from "antd";
 import Sider from "antd/es/layout/Sider";
 import { Content, Header } from "antd/es/layout/layout";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useRef, useState } from "react";
 import {
   TrophyOutlined,
   RocketOutlined,
@@ -21,100 +21,123 @@ import { usePathname, useRouter } from "next/navigation";
 import { CsvDataProvider } from "@/contexts/CsvDataProvider";
 import Link from "next/link";
 import { Logo } from "./Logo";
+import supabase from "@/hooks/supabaseConfig";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Session } from "@supabase/supabase-js";
 
 export default function NavBar(props: PropsWithChildren) {
   const pathname = usePathname();
   const router = useRouter();
 
-  // TODO: Handle authentication
-  // const { user } = useAuth();
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      if (!session) {
+        router.push("/login");
+      }
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
 
   const {
     token: { colorBgContainer },
   } = theme.useToken();
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Header
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          background: colorBgContainer,
-          padding: "0 20px",
-        }}
-      >
-        <Space>
-          <Logo />
-
-          <Typography.Title
+    <>
+      {session ? (
+        <Layout style={{ minHeight: "100vh" }}>
+          <Header
             style={{
-              fontSize: "inherit",
-              marginBottom: 0,
-              fontWeight: 700,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              background: colorBgContainer,
+              padding: "0 20px",
             }}
           >
-            OCOTOFORM
-          </Typography.Title>
-        </Space>
-        <Avatar style={{ backgroundColor: "#f12333" }}>U</Avatar>
-      </Header>
-      <Layout>
-        <Sider collapsible>
-          <Menu
-            theme="dark"
-            mode="inline"
-            selectedKeys={[pathname.split("/")[1]]}
-            items={[
-              {
-                key: "",
-                label: "Programs",
-                icon: <RocketOutlined />,
-              },
-              {
-                key: "activities",
-                label: "Activities",
-                icon: <TrophyOutlined />,
-              },
-              {
-                key: "surveys",
-                label: "Surveys",
-                icon: <ProfileOutlined />,
-              },
-              {
-                key: "test",
-                label: "Test",
-                icon: <ProfileOutlined />,
-              },
-              {
-                key: "test1",
-                label: "Test1",
-                icon: <ProfileOutlined />,
-              },
-            ]}
-            onClick={(e) => router.push("/" + e.key)}
-          />
-        </Sider>
-        <CsvDataProvider>
-          <Content style={{ padding: "20px" }}>
-            <Space
-              direction="vertical"
-              size="large"
-              style={{ display: "flex" }}
-            >
-              <Breadcrumb
-                items={pathname.split("/").map((item, i) => {
-                  if (i == pathname.split("/").length - 1) {
-                    return { title: item };
-                  }
-                  return { title: <Link href={"/" + item}>{item}</Link> };
-                })}
-              />
-              {props.children}
+            <Space>
+              <Logo />
+              <Typography.Title
+                style={{
+                  fontSize: "inherit",
+                  marginBottom: 0,
+                  fontWeight: 700,
+                }}
+              >
+                OCOTOFORM
+              </Typography.Title>
             </Space>
-          </Content>
-        </CsvDataProvider>
-      </Layout>
-    </Layout>
+            <Avatar style={{ backgroundColor: "#f12333" }}>U</Avatar>
+          </Header>
+          <Layout>
+            <Sider collapsible>
+              <Menu
+                theme="dark"
+                mode="inline"
+                selectedKeys={[pathname.split("/")[1]]}
+                items={[
+                  {
+                    key: "",
+                    label: "Programs",
+                    icon: <RocketOutlined />,
+                  },
+                  {
+                    key: "activities",
+                    label: "Activities",
+                    icon: <TrophyOutlined />,
+                  },
+                  {
+                    key: "surveys",
+                    label: "Surveys",
+                    icon: <ProfileOutlined />,
+                  },
+                  {
+                    key: "test",
+                    label: "Test",
+                    icon: <ProfileOutlined />,
+                  },
+                  {
+                    key: "test1",
+                    label: "Test1",
+                    icon: <ProfileOutlined />,
+                  },
+                ]}
+                onClick={(e) => router.push("/" + e.key)}
+              />
+            </Sider>
+            <CsvDataProvider>
+              <Content style={{ padding: "20px" }}>
+                <Space
+                  direction="vertical"
+                  size="large"
+                  style={{ display: "flex" }}
+                >
+                  <Breadcrumb
+                    items={pathname.split("/").map((item, i) => {
+                      if (i == pathname.split("/").length - 1) {
+                        return { title: item };
+                      }
+                      return { title: <Link href={"/" + item}>{item}</Link> };
+                    })}
+                  />
+                  {props.children}
+                </Space>
+              </Content>
+            </CsvDataProvider>
+          </Layout>
+        </Layout>
+      ) : (
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      )}
+    </>
   );
 }
