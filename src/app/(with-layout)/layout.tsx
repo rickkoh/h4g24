18ec40eeae1a67efbdb9bba1444f47676e490ed3
Rick <rick.kohjiaxuan@gmail.com>
2024-02-10 00:12:24 +0800

    Add AuthProvider to RootLayout in layout.tsx

--- src/app/(with-layout)/layout.tsx
+++ src/app/(with-layout)/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import "../globals.css";
 import NavBar from "@/components/NavBar";
+import { AuthProvider } from "@/contexts/AuthProvider";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 export default function RootLayout({
@@ -12,7 +13,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AntdRegistry>
-          <NavBar>{children}</NavBar>
+          <AuthProvider>
+            <NavBar>{children}</NavBar>
+          </AuthProvider>
         </AntdRegistry>
       </body>
     </html>
