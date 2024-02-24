9d84decd4dfa85c36acacff12b9fb51c16b32fe3
Rick <rick.kohjiaxuan@gmail.com>
2024-02-24 10:21:04 +0800

    Update activity page components and data fetchingfunctions

--- src/app/(with-layout)/activities/[id]/page.tsx
+++ src/app/(with-layout)/activities/[id]/page.tsx
@@ -1,6 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Button, Card, Col, Collapse, Row, Space, Spin, Table } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  Collapse,
+  Row,
+  Space,
+  Spin,
+  Table,
+  Typography,
+} from "antd";
 import WordCloud from "@/components/chart/WordCloud";
 import {
   RobotOutlined,
@@ -8,13 +18,15 @@ import {
   MehTwoTone,
   FrownTwoTone,
 } from "@ant-design/icons";
-import { GetAllForms } from "@/hooks/supabaseHooks";
-import { Survey } from "@/types/types";
+import { GetActivityById, GetFormByActivityId } from "@/hooks/supabaseHooks";
+import { Activity, Survey } from "@/types/types";
 import { useRouter } from "next/navigation";
 import Paragraph from "antd/es/typography/Paragraph";
+import { format } from "date-fns";
 
-// See where the activity data is fetched
-// fakeData should be replaced with the attribute called "ai_analysis" from the activity.
+// TODO: fakeData should be replaced with the attribute called "ai_analysis" from the activity.
+// Once you have updated the Activity type, you can replace the fakeData with the actual data.
+// E.g. activity.ai_analysis.summary etc
 const fakeData = {
   summary:
     "Participants enjoyed the well-structured and socially interactive Plants Growing activity, appreciating clear instructions that facilitated seamless participation. The positive feedback indicated an 88.2% likelihood of future engagement in similar activities. Despite this, 71.6% perceived the information source negatively.\n\nFavorite aspects included structured nature (85%), clear instructions (90%), peaceful environment (80%), and social interactions with friends (70%). However, the duration received a lower score at 40%.\n\nOverall, participants favored structured activities with clear guidance and social elements, suggesting a high interest in future engagements. To enhance satisfaction, improvements in information dissemination methods are recommended. The mixed perception of the information source underscores the need for effective communication strategies. Moreover, balancing different activity aspects, including appropriate durations, is crucial to maximize enjoyment and engagement levels.",
@@ -66,53 +78,34 @@ const fakeData = {
   },
 };
 
-interface Test {
-  text: string;
-  value: number;
-}
-
-const test: Test[] = [
+const tableColumns = [
   {
-    text: "structured",
-    value: 0.8,
+    title: "Title",
+    dataIndex: "title",
+    key: "title",
   },
   {
-    text: "flow",
-    value: 0.7,
+    title: "Date created",
+    dataIndex: "created_at",
+    key: "created_at",
   },
   {
-    text: "friends",
-    value: 0.6,
-  },
-  {
-    text: "interaction",
-    value: 0.65,
-  },
-  {
-    text: "instructors",
-    value: 0.85,
-  },
-  {
-    text: "clear",
-    value: 0.9,
-  },
-  {
-    text: "easy",
-    value: 0.75,
-  },
-  {
-    text: "peace",
-    value: 0.7,
-  },
-  {
-    text: "planting",
-    value: 0.8,
-  },
-  {
-    text: "duration",
-    value: 0.4,
+    title: "Last updated",
+    dataIndex: "updated_at",
+    key: "updated_at",
   },
 ];
+
+function mapKeywordsToWordCloudData(
+  keywords: { name: string; significance: number }[]
+) {
+  return keywords.map((keyword) => {
+    return {
+      text: keyword.name,
+      value: keyword.significance,
+    };
+  });
+}
 
 function SentimentalIcon({ label }: { label: string }) {
   switch (label) {
@@ -153,29 +146,29 @@ function SentimentalIcon({ label }: { label: string }) {
   }
 }
 
-const tableColumns = [
-  {
-    title: "Title",
-    dataIndex: "title",
-    key: "title",
-  },
-  {
-    title: "Date created",
-    dataIndex: "created_at",
-    key: "created_at",
-  },
-  {
-    title: "Last updated",
-    dataIndex: "updated_at",
-    key: "updated_at",
-  },
-];
-
 export default function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
+
+  const [activity, setActivity] = useState<Activity>();
+
+  useEffect(() => {
+    fetchActivityData();
+  }, []);
+
+  function fetchActivityData() {
+    setIsLoading(true);
+    GetActivityById({ activityId: id }).then((data) => {
+      if (data) {
+        setActivity(data);
+      }
+      setIsLoading(false);
+    });
+  }
+
   const { id } = params;
   const [allSurveysData, setAllSurveysData] = useState<Survey[]>();
   const [isLoading, setIsLoading] = useState(false);
+  const [isTableLoading, setIsTableLoading] = useState(false);
 
   const [isAIAnalysisLoading, setIsAIAnalysisLoading] = useState(false);
 
@@ -184,13 +177,12 @@ export default function Page({ params }: { params: { id: string } }) {
   }, []);
 
   function fetchSurveyData() {
-    // TODO: Change this to get survey data specific to the activity
-    setIsLoading(true);
-    GetAllForms().then((data) => {
+    setIsTableLoading(true);
+    GetFormByActivityId({ activityId: id }).then((data) => {
       if (data) {
         setAllSurveysData(data);
       }
-      setIsLoading(false);
+      setIsTableLoading(false);
     });
   }
 
@@ -207,6 +199,8 @@ export default function Page({ params }: { params: { id: string } }) {
     }).then((res) => {
       // Update
       setIsAIAnalysisLoading(false);
+
+      fetchActivityData();
 
       // Currently commented out because response is not showing
       // We just assume it's successful
@@ -241,22 +235,72 @@ export default function Page({ params }: { params: { id: string } }) {
         <div className="site-card-wrapper">
           <Row gutter={[16, 16]}>
             <Col span={6}>
-              <Card title="Total number of surveys" bordered={false}></Card>
+              <Card title="Title" bordered={false}>
+                <Space
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    justifyItems: "center",
+                  }}
+                >
+                  <Typography.Title level={1}>
+                    {activity?.title}
+                  </Typography.Title>
+                </Space>
+              </Card>
             </Col>
             <Col span={6}>
-              <Card title="Total number of responses" bordered={false}></Card>
+              <Card title="Total number of surveys" bordered={false}>
+                <Space
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    justifyItems: "center",
+                  }}
+                >
+                  <Typography.Title level={1}>
+                    {allSurveysData?.length}
+                  </Typography.Title>
+                </Space>
+              </Card>
             </Col>
             <Col span={6}>
-              <Card title="Keyword Analysis" bordered={false}></Card>
+              <Card title="Total number of responses" bordered={false}>
+                <Space
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    justifyItems: "center",
+                  }}
+                >
+                  <Typography.Title level={1}>120</Typography.Title>
+                </Space>
+              </Card>
             </Col>
             <Col span={6}>
-              <Card title="Keyword Analysis" bordered={false}></Card>
+              <Card title="Date Created" bordered={false}>
+                <Space
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    justifyItems: "center",
+                  }}
+                >
+                  <Typography.Title level={1}>
+                    {activity?.created_at
+                      ? format(activity!.created_at, "dd/MM/yyyy")
+                      : ""}
+                  </Typography.Title>
+                </Space>
+              </Card>
             </Col>
             <Col span={8}>
               <Card title="AI Summarised Findings" bordered={false}>
                 <div className="max-h-64 pt-8 overflow-scroll flex justify-center items-center">
                   {fakeData.summary ? (
                     <Paragraph>{fakeData.summary}</Paragraph>
+                  ) : isAIAnalysisLoading ? (
+                    <Spin />
                   ) : (
                     <Button
                       type="primary"
@@ -273,7 +317,13 @@ export default function Page({ params }: { params: { id: string } }) {
               <Card title="Keyword Analysis" bordered={false}>
                 <div className="max-h-64 overflow-scroll flex justify-center items-center">
                   {fakeData.keywords ? (
-                    <WordCloud data={test} width={256} height={256} />
+                    <WordCloud
+                      data={mapKeywordsToWordCloudData(fakeData.keywords)}
+                      width={256}
+                      height={256}
+                    />
+                  ) : isAIAnalysisLoading ? (
+                    <Spin />
                   ) : (
                     <Button
                       type="primary"
@@ -297,6 +347,8 @@ export default function Page({ params }: { params: { id: string } }) {
                 >
                   {fakeData.sentiments ? (
                     <SentimentalIcon label={fakeData.sentiments.label} />
+                  ) : isAIAnalysisLoading ? (
+                    <Spin />
                   ) : (
                     <Button
                       type="primary"
@@ -312,34 +364,24 @@ export default function Page({ params }: { params: { id: string } }) {
           </Row>
           <Row gutter={[16, 16]} style={{ marginTop: 20 }}>
             <Col span={24}>
-              {/* <Table
-            dataSource={Array.from(formResponses, ([question, responses]) => {
-              return {
-                question: question.text,
-                responses: responses
-                  .map((response) => response.answer)
-                  .join(", "),
-              };
-            })}
-            scroll={{ x: "max-content" }}
-          >
-            <Column title="Question" dataIndex="question" key="question" />
-            <Column title="Responses" dataIndex="responses" key="responses" />
-          </Table> */}
+              {isTableLoading ? (
+                <Spin />
+              ) : (
+                <Table
+                  dataSource={allSurveysData}
+                  columns={tableColumns}
+                  onRow={(record, rowIndex) => {
+                    return {
+                      onClick: (event) => {
+                        router.push(`/surveys/${record.id}`);
+                      },
+                    };
+                  }}
+                />
+              )}
             </Col>
           </Row>
         </div>
-        <Table
-          dataSource={allSurveysData}
-          columns={tableColumns}
-          onRow={(record, rowIndex) => {
-            return {
-              onClick: (event) => {
-                router.push(`/surveys/${record.id}`);
-              },
-            };
-          }}
-        />
       </Space>
     </main>
   );
